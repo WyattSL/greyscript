bcaeb8f34598c9754e3e26c41a9ec7fa45eb8b6c
ayecue <soerenwehmeier@googlemail.com>
2022-01-15 19:41:37 +0100

    2.0.0 change diagnostic to check for syntax as well

--- src/diagnostic.ts
+++ src/diagnostic.ts
@@ -3,43 +3,121 @@ import vscode, {
     Range,
     Diagnostic,
     TextDocumentChangeEvent,
-    TextDocument
+    TextDocument,
+    Position
 } from 'vscode';
-import Encryption from './grammar/Encryption.json';
+import { Encryption } from './grammar';
+import {
+    ASTBase,
+    ASTAssignmentStatement,
+    ASTIdentifier,
+    ASTFunctionStatement,
+    ASTCallStatement,
+    ASTCallExpression,
+    Parser
+} from 'greybel-core';
 
-function lookupErrors(source: string): Diagnostic[] {
+function getEncryptionCallName(item: ASTBase): string | undefined {
+    let expression;
+
+    if (item.type === 'CallStatement') {
+        expression = (item as ASTCallStatement).expression as ASTCallExpression;
+    } else if (item.type === 'CallExpression') {
+        expression = item as ASTCallExpression;
+    }
+
+    if (expression) {
+        const identifier = expression.base as ASTIdentifier;
+
+        if (Encryption.includes(identifier.name)) {
+            return identifier.name;
+        }
+    }
+
+    return;
+}
+
+function lookupErrors(document: TextDocument): Diagnostic[] {
+    const source = document.getText();
+    const parser = new Parser(source);
     const result = [];
-    const expr = new RegExp(`$(Encode|Decode)(?:\\s)?=(?:\\s)?function\\(.+\\).*(${Encryption.join("|")}).*end function`, "s");
-    const match = source.match(expr);
 
-    if (match) {
-        let s = source.indexOf(match[2]);
-        let e = source.indexOf(match[2]) + match[2].length;
-        let li = source.slice(0, s).split("/n").length;
-        let eli = source.slice(e, source.length).split("/n").length;
-        let max = source.slice(0, s);
-        let max2 = source.slice(0, e);
-        let sch = max.slice(max.lastIndexOf("/n"), max.indexOf(match[2])).length;
-        let ech = max2.slice(max2.lastIndexOf("/n"), max2.indexOf(match[2])+match[2].length).length;
-        let r = new Range(li, 1, eli, 99999)
-        let ms = "Cannot use "+match[2]+" in "+ (match[1] == "Encode" ? "encryption." : "decryption.");
-        let d = new Diagnostic(r, ms, vscode.DiagnosticSeverity.Error);
-        result.push(d);
+    try {
+        const chunk = parser.parseChunk();
+
+        //check for encryption
+        chunk.body.forEach((item) => {
+            if (item.type === 'AssignmentStatement') {
+                const { init, variable } = item as ASTAssignmentStatement;
+                const left = variable.type === 'Identifier' ? variable as ASTIdentifier : null;
+                const right = init.type === 'FunctionDeclaration' ? init as ASTFunctionStatement : null;
+
+                if (
+                    left &&
+                    right &&
+                    /^(Encode|Decode)$/.test(left.name)
+                ) {
+                    right.body.forEach((innerItem) => {
+                        const name = getEncryptionCallName(innerItem);
+
+                        console.log(name, innerItem);
+
+                        if (name) {
+                            result.push(
+                                new Diagnostic(
+                                    document.lineAt(innerItem.line - 1).range,
+                                    `Cannot use ${name} in ${left.name}`,
+                                    vscode.DiagnosticSeverity.Error
+                                )
+                            );
+                        } else if (innerItem.type === 'AssignmentStatement') {
+                            const rightName = getEncryptionCallName((innerItem as ASTAssignmentStatement).init);
+
+                            result.push(
+                                new Diagnostic(
+                                    document.lineAt(innerItem.line - 1).range,
+                                    `Cannot use ${rightName} in ${left.name}`,
+                                    vscode.DiagnosticSeverity.Error
+                                )
+                            );
+                        }
+                    });
+                }
+            }
+        });
+    } catch (err: any) {
+        vscode.window.showErrorMessage(err.message, { modal: false });
+
+        let line;
+
+        if (err.hasOwnProperty('line')) {
+            line = err.line;
+        } else if (err.hasOwnProperty('token')) {
+            line = err.token.line;
+        }
+
+        result.push(
+            new Diagnostic(
+                document.lineAt(line - 1).range,
+                err.message,
+                vscode.DiagnosticSeverity.Error
+            )
+        );
     }
 
     return result;
 }
 
 export function activate(context: ExtensionContext) {
-    const diagnosticCollection = vscode.languages.createDiagnosticCollection("greyscript");
+    const collection = vscode.languages.createDiagnosticCollection("greyscript");
 
     function updateDiagnosticCollection(document: TextDocument) {
-        const err = lookupErrors(document.getText());
-        diagnosticCollection.set(document.uri, err);
+        const err = lookupErrors(document);
+        collection.set(document.uri, err);
     }
 
 	context.subscriptions.push(
-        diagnosticCollection,
+        collection,
         vscode.workspace.onDidOpenTextDocument(updateDiagnosticCollection),
         vscode.workspace.onDidChangeTextDocument((event: TextDocumentChangeEvent) => {
             updateDiagnosticCollection(event.document);
